8d961459a6b2c12ab2195171387913220990d47b
alexsavio <alexsavio@gmail.com>
2024-04-03 13:18:53 +0200

    db: add migration to set subscriptions.status not null

---
+++ migrations/20240403111607_make_status_not_null_in_subscriptions.sql
@@ -0,0 +1,5 @@
+-- Add migration script here
+BEGIN;
+    UPDATE subscriptions SET status = 'confirmed' WHERE status IS NULL;
+    ALTER TABLE subscriptions ALTER COLUMN status SET NOT NULL;
+COMMIT;
